57c0291bb88eb4a4cd73c6190b38321c349a687e
naoto <naotofujiwara890@gmail.com>
2026-08-29 14:31:23 +0900

    chapter08 omake

--- chapter08/omake.cpp
+++ chapter08/omake.cpp
@@ -4,7 +4,9 @@
 
 int main(){
     const std::string& hoge="Hello";//Helloはprvalueで、hogeはconst lvalue referenceで受け取るので、Helloの寿命が延長される
-
     std::cout<<hoge<<std::endl;
+
+    std::string fuga="Hello";//Helloはprvalueで、fugaはlvalueなので、Helloの寿命は延長されない
+    std::cout<<fuga<<std::endl;
 
 }
