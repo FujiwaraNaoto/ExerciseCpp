11c4d082f33077796b3c69ff86e6be9237210daf
naoto <naotofujiwara890@gmail.com>
2026-09-25 22:55:41 +0900

    is_lock_freeを試す

---
+++ chapter15/std_atomic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <atomic>
+
+template <typename T>
+void print_lock_free(){
+    std::atomic<T> a;
+    std::cout<< "a.is_always_lock_free=" << a.is_always_lock_free << std::endl;
+    std::cout<< "a.is_lock_free()=" << a.is_lock_free() << std::endl;
+}
+
+template <typename... Ts>
+void print_lock_free_all(){
+    (print_lock_free<Ts>(), ...);
+}
+
+int main(){
+
+    std::atomic<int> counter;
+    std::cout<< "counter.is_always_lock_free = " << counter.is_always_lock_free << std::endl;
+    std::cout<< "counter.is_lock_free()=" << counter.is_lock_free() << std::endl;
+
+
+    print_lock_free_all<char, short, int, long>();
+
+}
