aeabb7b0c9298aa6f08d4986939fb8bd5704e218
naoto <naotofujiwara890@gmail.com>
2026-09-25 17:58:09 +0900

    base

---
+++ chapter15/an-overloaded-container.cpp
@@ -0,0 +1,22 @@
+#include<initializer_list>
+#include<iostream>
+
+struct Container
+{
+    Container(int,int)
+    {
+        std::cout<<"Two ints\n";
+    }
+
+    Container(std::initializer_list<float> )
+    {
+        std::cout<<"std::initializer_list<float>\n";
+    }
+};
+
+int main()
+{
+    Container c1(1,2);
+    Container c2{1,2};
+    return 0;
+}
